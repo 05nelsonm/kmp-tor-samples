ab251af0516e4f222622c85ac9175d0ffaeb4d39
Matthew Nelson <developer@matthewnelson.io>
2024-08-25 05:38:13 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# kmp-tor-samples
+[![badge-license]][url-license]
+
+Samples for [kmp-tor][url-kmp-tor]
+
+### Applications
+
+ - [compose](samples/compose/README.md): Android, iOS, Jvm Desktop
+
+[badge-license]: https://img.shields.io/badge/license-Apache%20License%202.0-blue.svg?style=flat
+
+[url-license]: https://www.apache.org/licenses/LICENSE-2.0
+[url-kmp-tor]: https://github.com/05nelsonm/kmp-tor
---
+++ settings.gradle.kts
@@ -0,0 +1,38 @@
+@file:Suppress("UnstableApiUsage")
+
+rootProject.name = "kmp-tor-samples"
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
+
+includeBuild("build-logic")
+
+listOf(
+    "compose",
+).forEach { module ->
+    include(":samples:$module")
+}
